4bbded90a91a0a79cb5a7c00b5c59130b87ed6a7
Martin Fekete <martinfekete10@gmail.com>
2024-06-12 20:09:53 +0200

    Initial

---
+++ Casks/tuneful.rb
@@ -0,0 +1,17 @@
+cask "tuneful" do
+  desc "Native macOS menu bar playback control app for Spotify and Apple Music"
+  homepage "https://github.com/martinfekete10/Tuneful"
+  url "https://github.com/martinfekete10/Tuneful/releases/download/v1.2.0/Tuneful.tar.gz"
+  sha256 "f360663290d0aeb01ab23112c508819f1ab7931b2ae196a8472a1433ad41cfd8"
+  version "1.2.0"
+
+  livecheck do
+    url "https://github.com/lihaoyun6/AirBattery/releases/latest"
+    strategy :page_match
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+  
+  depends_on macos: ">= :ventura"
+
+  app "AirBattery.app"
+end
