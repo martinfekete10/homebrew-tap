0418900eaf383b1b4f4be6db4b8ea18dadf148da
Martin Fekete <martinfekete10@gmail.com>
2024-06-12 22:41:17 +0200

    Newest version and dmg download

--- Casks/tuneful.rb
+++ Casks/tuneful.rb
@@ -1,34 +1,13 @@
 cask "tuneful" do
-  sha256 "f360663290d0aeb01ab23112c508819f1ab7931b2ae196a8472a1433ad41cfd8"
-  version "1.2.0"
+  version "1.5.0"
+  sha256 "a85a13a79bd9eeae7f227ceb355b3f60a3a855fba38c2f41bedfa54dcbdf519f"
 
   desc "Native macOS menu bar playback control app for Spotify and Apple Music"
   homepage "https://github.com/martinfekete10/Tuneful"
-  url "https://github.com/martinfekete10/Tuneful/releases/download/v1.2.0/Tuneful.tar.gz"
+  url "https://github.com/martinfekete10/Tuneful/releases/download/v#{version}/Tuneful.dmg"
 
   depends_on macos: ">= :ventura"
   app "Tuneful.app"
-
-  # livecheck do
-  #   url "https://github.com/martinfekete10/Tuneful/releases/latest"
-  #   strategy :page_match
-  #   regex(%r{v?(\d+(?:\.\d+)+)\s*-\s*.*}i)
-  # end
-
-  # livecheck do
-  #   url :stable
-  #   regex(/^v?(\d+(?:\.\d+)+)*$/i)
-  #   strategy :github_releases do |json, regex|
-  #     json.map do |release|
-  #       next if release["draft"] || release["prerelease"]
-  
-  #       match = release["tag_name"]&.match(regex)
-  #       next if match.blank?
-  
-  #       match[1]
-  #     end
-  #   end
-  # end
 
   livecheck do
     url :url
